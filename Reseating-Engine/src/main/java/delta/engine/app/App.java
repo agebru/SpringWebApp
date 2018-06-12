0db912b5faa5c5380f4dc21f6193d1624e5bf13b
agebru <amex2000@gmail.com>
2018-06-11 20:22:00 -0400

    adding DateFormatters

---
+++ Reseating-Engine/src/main/java/delta/engine/app/App.java
@@ -0,0 +1,66 @@
+package delta.engine.app;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
+import delta.engine.dao.FlightStore;
+import delta.engine.model.Flight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       List<Flight> flightData=FlightStore.loadAllFligths();
+       
+       // all flights
+       System.out.println("All Flights:\n ========================================\n");
+       flightData.stream().forEach(System.out::println);
+       
+       //only flights to Atlanta
+       System.out.println("\n=======================================================================================================\n\n"
+       		+ " Flights to Atlanta\n ========================================\n");
+       flightData.stream()
+       .filter(fl->fl.getTo().equals("Atlanta"))
+       .forEach(System.out::println);
+       
+       
+       System.out.println("\n\n Printing Date Formats");
+       
+       //Note: Month should be in Capital==>MM
+       DateFormat sdf=new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+         Date now=new Date();
+       String mydf=sdf.format(now);
+       
+       System.out.println("Today's Date is ===> : "+mydf);
+       
+       
+       
+       /// Date Conversion
+       LocalDateTime toDay = LocalDateTime.now();
+       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+       System.out.println("Today's Date: "+toDay.format(formatter));
+       
+       
+       
+       DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+       LocalDate ldate=LocalDate.of(1989, Month.MAY, 21);       
+       System.out.println("Today's LocalDate: "+ldate.format(formatter2));
+       
+       
+         
+       
+       
+       
+       
+    }
+}
---
+++ Reseating-Engine/src/main/java/delta/engine/dao/FlightStore.java
@@ -0,0 +1,69 @@
+package delta.engine.dao;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import delta.engine.model.Flight;
+
+public class FlightStore {
+	private static List<Flight> flights = new ArrayList<>();
+	
+	private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+	private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	
+	
+
+	private FlightStore() {
+
+	}
+
+	static {
+		Flight flight1 = new Flight(1001, "Delta01", "Chicago", "Atlanta", LocalDate.now().atTime(9, 30),
+				LocalDate.now().atTime(10, 30));
+
+		Flight flight2 = new Flight(1104, "Delta30", "Columbus", "Maryland", LocalDate.now().atTime(11, 35),
+				LocalDate.now().atTime(12, 30));
+		Flight flight3 = new Flight(1220, "Delta40", "Lost Angelos", "Iowa", LocalDate.now().atTime(8, 30),
+				LocalDate.now().atTime(10, 30));
+		Flight flight4 = new Flight(1031, "Delta66", "New York", "DC", LocalDate.now().atTime(1, 40),
+				LocalDate.now().atTime(3, 30));
+		Flight flight5 = new Flight(1079, "Delta32", "Chicago", "Atlanta", LocalDate.now().atTime(4, 10),
+				LocalDate.now().atTime(6, 45));
+
+		flights.add(flight1);
+		flights.add(flight2);
+		flights.add(flight3);
+		flights.add(flight4);
+		flights.add(flight5);
+
+	}
+
+	public static List<Flight> loadAllFligths() {
+		List<Flight> tempFlights=new ArrayList<>();
+		for(Flight f:flights) {
+			f.getStartTime().format(dtf);
+			f.getDestinationTime().format(dtf);			
+			tempFlights.add(f);
+			
+		}
+		
+		return tempFlights;
+	}
+
+	public static Flight findFlight(int flightNumber) {
+		return flights.stream()
+				      .filter(flyt -> flyt.getFlightNumber() == flightNumber)
+				      .findFirst().get();
+	}
+	
+	public static Flight findFlightByName(String name) {
+		return flights.stream()
+				      .filter(flyt -> flyt.getName().equals(name))
+				      .findFirst().get();
+	}
+
+}
